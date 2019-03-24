b6201c1af38ac144e5e13985166a22b40504b724
Eric Swanson <eswanson@alloscomp.com>
2019-03-24 13:08:21 -0700

    RUST

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-fn main() {
-    println!("Hello, world: %d", fake());
-}
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
-fn fake() -> i16 {
-    return 42
+fn main() -> std::io::Result<()> {
+    let file = File::open("foo.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    assert_eq!(contents, "Hello, world!");
+    Ok(())
 }
