d2789937d25c27c9b21772f065ef056bd2e71d93
Eric Swanson <eswanson@alloscomp.com>
2019-03-24 13:27:06 -0700

    Single hasher

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ use std::fs::File;
 use std::io::Read;
 use std::io::BufReader;
 
+extern crate crypto;
+
+use self::crypto::digest::Digest;
+use self::crypto::sha2::Sha256;
+
 fn main() -> std::io::Result<()> {
-    let input = std::io::stdin();
-    let mut buf_reader = BufReader::new(input);
+    let mut buf_reader = BufReader::new(std::io::stdin());
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
-    print!("{:}", contents);
+    let mut hasher = Sha256::new();
+    hasher.input_str(&contents);
+    let hex = hasher.result_str();
+    println!("sha256: {}", hex);
     Ok(())
 }
