d8e1eb417c8424b0606786b683cea7904d49e18c
Zachary Vance <za3k@za3k.com>
2019-03-24 13:26:51 -0700

    Finish cat

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
+use std::io::Read;
 use std::io::BufReader;
-use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("foo.txt")?;
-    let mut buf_reader = BufReader::new(file);
+    let input = std::io::stdin();
+    let mut buf_reader = BufReader::new(input);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
-    assert_eq!(contents, "Hello, world!");
+    print!("{:}", contents);
     Ok(())
 }
