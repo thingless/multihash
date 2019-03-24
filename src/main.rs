098b17c1b1938bdbb735c5fe48b2e95cba4a2282
Zachary Vance <za3k@za3k.com>
2019-03-24 14:16:12 -0700

    working multihash?

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,31 @@ extern crate crypto;
 
 use self::crypto::digest::Digest;
 use self::crypto::sha2::Sha256;
+use self::crypto::sha2::Sha224;
 
 fn main() -> std::io::Result<()> {
     let mut stdin = std::io::stdin();
+    let mut hashers : [Box<Digest>; 2] = [
+        Box::new(Sha256::new()), 
+        Box::new(Sha224::new())
+    ];
     let mut buf: [u8; 1024] = [0; 1024];
 
-    let mut hasher = Sha256::new();
     loop {
         match stdin.read(&mut buf) {
             Ok(0) => break,
-            Ok(v) => hasher.input(&buf[0..v]),
-            Err(_) => break,
+            Err(e) => panic!(e),
+            Ok(v) => {
+                for hasher in hashers.iter_mut() {
+                    hasher.input(&buf[0..v]);
+                }
+            },
         }
     }
 
-    let hex = hasher.result_str();
-    println!("sha256: {}", hex);
+    for hasher in hashers.iter_mut() {
+        let hex = hasher.result_str();
+        println!("some hash: {}", hex);
+    }
     Ok(())
 }
